552c74bab5b1672b78c7b5cbaebb0c6ec2217594
krishna kant singh <krishnakant.singh14@gmail.com>
2025-12-26 16:50:12 +0100

    Create 1D Heat Equation using Span subspan, views and ranges.

--- Lec1/1DHeatEquation/heatwithspan.cpp
+++ Lec1/1DHeatEquation/heatwithspan.cpp
@@ -15,7 +15,19 @@ void gaussian_initial_condition(std::span<double> uspan, double L, double dx) {
             return std::exp(-std::pow(x - L/2.0, 2));
         }
     );
+    
     std::ranges::copy(uview, uspan.begin());
+
+}
+
+
+void simulation_step(std::span<double> uspan, double alpha) {
+    auto sim_view = std::views::adjacent<3>(uspan) | std::views::transform([alpha] (auto window) {
+        auto [p, c, n] = window;
+        return c + alpha*(n-2.0*c + p);
+    });
+    std::ranges::copy(sim_view, uspan.begin()+1);
+
 }
 
 
@@ -30,7 +42,21 @@ int main() {
     const int timeSteps = 1000;
 
     std::vector<double> u(N,0);
+    std::vector<double> unew(N,0);
+    std::span<double> uspan(u);
 
-    gaussian_initial_condition(u, L, dx);    
+
+    gaussian_initial_condition(u, L, dx);   
+    auto middle = uspan.subspan(48,52);
+    gaussian_initial_condition(middle, 0.1, dx);   
     std::cout << "Temperature at center: " << u[N/2] << std::endl;
+    std::cout << "Temperature at start: " << u[1] << std::endl;
+    std::cout << "Temperature at last: " << u[N-1] << std::endl;
+ 
+
+    for (int t=0; t<timeSteps; ++t) {
+        simulation_step(uspan, alpha);
+        std::cout << "Temperature at center: " << u[N/2] << std::endl;
+
+    }
 }
